5ac2c0521d7c01410e398a13212dc657aa77fe47
KiriXen <kirixenyt@gmail.com>
2024-02-04 00:22:36 +0530

    Update popular.php

--- popular.php
+++ popular.php
@@ -201,7 +201,7 @@ if ($page == ""){
     <div class="clr"></div>
     <div class="mask"></div>
         <script type="text/javascript" src="<?=$base_url?>/js/files/combo.js"></script>
-    <script type="text/javascript" src="https://anikatsu.ga/files/js/video.js"></script>
+    <script type="text/javascript" src="http://kitsunime.unaux.com/files/js/video.js"></script>
     <script type="text/javascript" src="<?=$base_url?>/js/files/jquery.tinyscrollbar.min.js"></script>
     <?php include('/php/include/footer.php')?>
 
