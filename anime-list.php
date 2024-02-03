a36c2da8798bbfe71fec855760ff7f22b72c2177
KiriXen <kirixenyt@gmail.com>
2024-02-04 00:33:55 +0530

    Update anime-list.php

--- anime-list.php
+++ anime-list.php
@@ -60,7 +60,7 @@ if ($page == ""){
                                 <h2>ANIME LIST</h2>
                                 <div class="anime_name_pagination">
                                     <div class="pagination">
-                                    <ul class='pagination-list'><?php $pagination = file_get_contents("$apiLink/allAnimeListPage/$page");$pagination = json_decode($pagination, true); echo str_replace("active","selected",$pagination['pagination']) ?>
+                                    <ul class='pagination-list'><?php $pagination = file_get_contents("$apiLink/anime-list-page/$page");$pagination = json_decode($pagination, true); echo str_replace("active","selected",$pagination['pagination']) ?>
                                     </div>
                                 </div>
                             </div>
@@ -152,7 +152,7 @@ if ($page == ""){
                             <div class="anime_list_body">
                                 <ul class="listing">
                                 <?php
-                                  $json = file_get_contents("$apiLink/getAllAnime/$page");
+                                  $json = file_get_contents("$apiLink/animeList/$page");
                                   $json = json_decode($json, true);
                                   foreach($json as $animeList)  { 
                                 ?>
@@ -276,7 +276,7 @@ if ($page == ""){
     <div class="clr"></div>
     <div class="mask"></div>
         <script type="text/javascript" src="<?=$base_url?>/js/files/combo.js"></script>
-    <script type="text/javascript" src="https://anikatsu.ga/files/js/video.js"></script>
+    <script type="text/javascript" src="http://kitsunime.unaux.com/files/js/video.js"></script>
     <script type="text/javascript" src="<?=$base_url?>/js/files/jquery.tinyscrollbar.min.js"></script>
     <div class="notice-400"
     style=" z-index:99999;position: fixed;bottom: 0;text-align: center;width: 100%; left: 0;padding: 10px;background: #939393;color: white;">
