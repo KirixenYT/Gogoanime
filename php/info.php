91d35b54ad3fc0e23c2907524016626fcfa1776e
KiriXen <kirixenyt@gmail.com>
2024-02-04 00:17:30 +0530

    Update info.php

--- php/info.php
+++ php/info.php
@@ -1,5 +1,5 @@
 <?php 
 $base_url = "//{$_SERVER['SERVER_NAME']}";
 $website_name = "GogoAnime";
-$apiLink = "https://api.toontamilind.in/anime/gogoanime"; 
+$apiLink = "https://kitsunime-api.onrender.com"; 
 ?>
