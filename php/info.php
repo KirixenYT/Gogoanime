1208f13a57e8c892b9d556aa2f5d1c7ec9c92683
KiriXen <kirixenyt@gmail.com>
2024-03-03 20:22:20 +0530

    Delete php/info.php

--- php/info.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$base_url = "//{$_SERVER['SERVER_NAME']}/gogoanime";
-$website_name = "GogoAnime";
-$apiLink = "https://kitsunime-api.onrender.com"; 
-if (date("d") > 15){
-    $apiLink = "https://kitsunime-api.onrender.com";
-}
-?>
