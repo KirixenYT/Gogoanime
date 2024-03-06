b123aad6d3ea64a0f1e1fdfc0fe642961cb0beeb
KiriXen <kirixenyt@gmail.com>
2024-03-06 19:35:20 +0530

    Update info.php

--- php/info.php
+++ php/info.php
@@ -1,8 +1,8 @@
 <?php 
 $base_url = "//{$_SERVER['SERVER_NAME']}";
 $website_name = "GogoAnime";
-$apiLink = "https://animeapi-kirex.koyeb.app"; 
+$apiLink = ""; // add the api here with the '/' at the end!
 if (date("d") > 15){
-    $apiLink = "https://animeapi-kirex.koyeb.app";
+    $apiLink = ""; // same goes for here
 }
 ?>
