af98406c1e6d48b3be1724323ceaae6d6957e875
KiriXen <kirixenyt@gmail.com>
2024-05-19 15:08:08 +0530

    Update info.php

--- php/info.php
+++ php/info.php
@@ -2,6 +2,7 @@
 $base_url = "//{$_SERVER['SERVER_NAME']}";
 $website_name = "GogoAnime";
 $apiLink = ""; // add the api here with the '/' at the end!
+$consumet = ""; // host ur own consumet api from here `https://github.com/consumet/api.consumet.org`
 if (date("d") > 15){
     $apiLink = ""; // same goes for here
 }
